f568af3816e0318870c0394311e91a7dc0dc8183
Alex Shibalkin <shibalkun@ya.ru>
2024-02-28 19:52:21 +0500

    Добавлен проект тестов на Storage

---
+++ GigaApp.Storage.Tests/StorageTestFixture.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Testcontainers.MsSql;
+
+namespace GigaApp.Storage.Tests
+{
+    public class StorageTestFixture : IAsyncLifetime
+    {
+        private readonly MsSqlContainer msSqlContainer = new MsSqlBuilder().Build();
+
+
+        public ForumDbContext GetDbContext()
+        {
+            return new ForumDbContext(new DbContextOptionsBuilder<ForumDbContext>()
+                .UseSqlServer(msSqlContainer.GetConnectionString()).Options);
+        }
+        
+
+        public async Task InitializeAsync()
+        {
+            await msSqlContainer.StartAsync();
+            var forumDbContext = new ForumDbContext(new DbContextOptionsBuilder<ForumDbContext>()
+                .UseSqlServer(msSqlContainer.GetConnectionString()).Options);
+
+            forumDbContext.Database.Migrate();
+        }
+
+        public new async Task DisposeAsync() => await msSqlContainer.DisposeAsync();
+    }
+}
